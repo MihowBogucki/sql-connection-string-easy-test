4f9d7b2c222f36dec599dc91b3a3c8fc6b6a0fc0
Mihow Bogucki <mihow.bogucki@gmail.com>
2021-05-26 21:01:55 +0200

    int

---
+++ SqlConnectionStringTester/ConnectionStringTester.cs
@@ -0,0 +1,46 @@
+﻿namespace SqlConnectionStringTester
+{
+    using System;
+    using System.Text.Json;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.Extensions.Logging;
+
+    public static class ConnectionStringTester
+    {
+        public static void Test(string connectionString)
+        {
+            var loggerFactory = LoggerFactory.Create(
+                builder =>
+                {
+                    builder.AddConsole();
+                });
+            
+            var logger = loggerFactory.CreateLogger("Program");
+
+            void Print(object o)
+            {
+                if (o is string s)
+                {
+                    logger.LogInformation(s);
+                }
+                else
+                {
+                    logger.LogInformation($"{JsonSerializer.Serialize(o)}");
+                }
+            }
+
+            try
+            {
+
+                using var conn = new SqlConnection(connectionString);
+                conn.Open();
+
+                Print($"Connection is working");
+            }
+            catch (Exception e)
+            {
+                Print($"Connection failed, {e}");
+            }
+        }
+    }
+}
